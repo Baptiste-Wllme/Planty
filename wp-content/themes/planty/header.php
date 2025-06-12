461a871d22edc2ea038597a81305d4f725e629c1
Baptiste Wllme <baptiste.willaume.pro@gmail.com>
2025-06-12 13:50:47 +0200

    banner

---
+++ wp-content/themes/planty/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+    
+    <head>
+    
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <?php wp_head(); ?>
+    
+    </head>
+
+    <body <?php body_class(); ?>>
+
+    <?php
+     wp_body_open(); 
+    ?>
+    
+<div id="wrapper" class="hfeed">
+
+<header id="header" role="banner" class="sticky-top">
+    <div id="branding">
+        <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+            <img src="wp-content/themes/planty/img/Logo.png" class="ImgLogo" alt="Logo Planty" />
+        </div>
+    </div>
+    <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+        
+    </nav>
+</header>
+
+<div id="container">
+<main id="content" role="main"> 
